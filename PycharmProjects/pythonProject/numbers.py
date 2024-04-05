5b606ee8912b1688acff5c2e2039e48c93a28bc1
Dipesh-Gautam <be2019se674@gces.edu.np>
2024-04-05 22:33:53 +0545

    pass

--- PycharmProjects/pythonProject/numbers.py
+++ PycharmProjects/pythonProject/numbers.py
@@ -1,5 +1,13 @@
 
-for i in range(1,101):
+for i in range(1,11):
     if(i%2!=0):
         continue
     print(i)
+    
+print(" ")
+
+for j in range(1,11):
+    if (j%2==0):
+        pass
+    else:
+        print(j)
